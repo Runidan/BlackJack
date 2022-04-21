3b03a2459fdb9270c299312dff63d31c1d993a57
Runidan <rudan@zlatoust.en.cx>
2022-04-21 19:52:28 +0500

    add add_card method

--- player.rb
+++ player.rb
@@ -9,21 +9,19 @@ class Player
     @cash = 100
   end
 
-  def getting_cards(deck)
-
+  def add_card(deck)
+    @cards << deck.take_card
   end
 
-  def count_card(player)
+  def count_card
     sum = 0
     card_price = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, J: 10, Q: 10, K: 10, A: 11 } 
-    ctr_sum -> (plr) do
-      plr.cards.each { |card| sum += card_price[card.rank] }
-    end 
-    ctr_sum.call(player)
+    carts_sum -> { @cards.each { |card| sum += card_price[card.rank] } }
+    carts_sum.call
     if sum > 21
       card_price[:A] = 1
       sum = 0
-      ctr_sum.call(player)
+      carts_sum.call
     end
     sum
   end
