eecf23c6696d102cfd5dcc74b4d78a127ba358de
Runidan <rudan@zlatoust.en.cx>
2022-04-21 19:45:54 +0500

    move count_card from casino to player

--- casino.rb
+++ casino.rb
@@ -1,26 +1,18 @@
 # frozen_string_literal: true
+require_relative 'player'
+require_relative 'dealer'
 
 class Casino
   def initialize
     print 'Введите ваше имя:'
     name = gets.chomp
     @player = Player.new(name)
-    @dialer = Dialer.new('Dealer')
+    @dialer = Dealer.new('dealer')
     @bank = 0
   end
 
-  def count_card(player)
-    sum = 0
-    card_price = { "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, J: 10, Q: 10, K: 10, A: 11 } 
-    ctr_sum -> (plr) do
-      plr.cards.each { |card| sum += card_price[card.rank] }
-    end 
-    ctr_sum.call(player)
-    if sum > 21
-      card_price[:A] = 1
-      sum = 0
-      ctr_sum.call(player)
-    end
-    sum
-  end
+
+
+  private
+
 end
