dd09bbf05f032040afa7acb945db72c6e0345ea8
Runidan <rudan@zlatoust.en.cx>
2022-04-21 23:30:05 +0500

    add special add_card for dealer

--- dealer.rb
+++ dealer.rb
@@ -3,4 +3,15 @@
 require_relative 'player'
 
 class Dealer < Player
+  def add_card(deck)
+    @cards << deck.take_card
+  end
+
+  def add_card(deck)
+    @cards << deck.take_card
+    @cards << deck.take_card
+    if count_card >= 17
+      @cards << deck.take_card
+    end
+  end
 end
